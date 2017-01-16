30049d2f60d8263ab69aa0884140f626561d488c
RookieDay <1109318841@qq.com>
2017-01-17 00:14:01 +0800

    update

--- vue/js/process.js
+++ vue/js/process.js
@@ -262,3 +262,54 @@ new Vue({
         counter: 0
     }
 })
+
+Vue.component('button-counter', {
+    template: '<button v-on:click="increment">{{counter}}</button>',
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter += 1;
+            this.$emit('increment');
+        }
+    }
+})
+
+new Vue({
+    el: '#counter-event-example',
+    data: {
+        total: 0
+    },
+    methods: {
+        incrementTotal: function() {
+            this.total += 1;
+        }
+    }
+})
+
+Vue.component('currency-input', {
+    template: '\
+    <span>\
+      $\
+      <input\
+        ref="input"\
+        v-bind:value="value"\
+        v-on:input="updateValue($event.target.value)"\
+      >\
+    </span>\
+  ',
+    props: ['value'],
+    methods: {
+        updateValue: function(value) {
+            var formattedValue = value.trim()
+                .slice(0, value.indexOf('.') + 3);
+            if (formattedValue !== value) {
+                this.$refs.input.value = formattedValue
+            }
+            this.$emit('input', Number(formattedValue))
+        }
+    }
+})
